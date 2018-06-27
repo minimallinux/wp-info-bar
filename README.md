f6ad8cdba50211e561d59626c9fe9cf7a453b3c8
minimallinux <anthonymcgowan1@gmail.com>
2018-06-27 10:31:48 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Info Bar Wordpress
+
+Universal Info Bar for Wordpress.
+
+## Quick Start
+
+Insert html code in to header.php and adjust to suit. Has a utility nav menu on left for extra items.
+Social icons on right and contact info in middle. (still in the right section)
+Can be used for any theme.
+Basic styles attached. See screenshot.
+
+## App Info
+
+Universal info bar for Wordpress top of site.
+
+### Author
+
+P A McGowan
+
+https://webmobapps.com
+https://minimallinux.tk
+
+### Version
+
+1.0.0
+
+### License
+
+This project is licensed under the MIT License
---
+++ info-bar.php
@@ -0,0 +1,29 @@
+<div class="info-bar">
+            <div class="container">
+              <div class="left-section">
+              	 <?php wp_nav_menu( array( 'theme_location' => 'top-bar' ) ); ?> 	
+              </div><!-- .left-section -->          
+            <div class="right-section">
+           <div class="social-icons">
+           	 <a class="icon-box-home" href="https://demo.proteusthemes.com/bolts/contact-us/#pg-31-1" target="_self">
+                <i class="fa fa-lg fa-home"></i></a>
+                <a title="Call Us" class="icon-box" href="tel:+447769776688" target="_self">
+                <i class="fa fa-lg  fa-phone"></i> +441709263467</a>
+                 <a title="Email Us" class="icon-box" href="mailto:sales@onestopglass.co.uk" target="_self">
+                <i class="fa fa-lg  fa-envelope"></i> sales@allseasons.co.uk</a>
+                <a title="Connect With Us" class="icon-box" href="skype:username?chat" target="_self">
+                <i class="fa fa-lg fa-skype"></i> Skype</a>
+                <a title="Write To Us" class="icon-box" href="address:+447769776688"><i class="fa fa-lg  fa-envelope"></i> 227 Marion Street, Columbia, SC 29201</a>
+            <a class="social-icons" href="https://www.facebook.com/ProteusThemes/" target="_self"><i class="fa   fa-lg fa-facebook"></i></a>
+            <a class="social-icons" href="https://twitter.com/ProteusThemes" target="_self"><i class="fa  fa-twitter"></i></a>
+            <a class="social-icons" href="https://twitter.com/ProteusThemes" target="_self"><i class="fa  fa-twitter"></i></a>
+            <a class="social-icons" href="https://linkedin.com" target="_self"><i class="fa fa-lg fa-linkedin"></i></a>
+            <a class="social-icons" href="https://plus.google.com" target="_self"><i class="fa fa-lg fa-google-plus"></i></a>
+            <a class="social-icons" href="https://rss.com" target="_self"><i class="fa  fa-rss"></i></a>
+            <a class="social-icons" href="https://pinterest.com" target="_self"><i class="fa fa-lg fa-pinterest"></i></a>
+    
+</div>  <!-- .social-profiles -->
+                        </div><!-- .right-section -->
+                
+            </div><!-- .container -->
+        </div><!-- .info-bar -->
